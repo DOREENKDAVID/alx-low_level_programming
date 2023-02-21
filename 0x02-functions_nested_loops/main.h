47e30aaae1b1d06a7c6cee7842dc4ac56cd17859
DOREENDAVID <doreenkinya153@gmail.com>
2023-02-21 11:06:24 -0800

    nested-loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,12 @@
-0. int _putchar(char c):
-1. void print_alphabet(void);
-2. void print_alphabet_x10(void);
-3. int _islower(int c);
-4. int _isalpha(int c);
-5. int print_sign(int n);
-6. int _abs(int);
-7. int print_last_digit(int);
-8. void jack_bauer(void);
-9 void times_table(void);
-10. int add(int, int);
-11. void print_to_98(int n);
-12. 
+1  int _putchar(char c):
+2  void print_alphabet(void);
+3  void print_alphabet_x10(void);
+4  int _islower(int c);
+5  int _isalpha(int c);
+6  int print_sign(int n);
+7  int _abs(int);
+8  int print_last_digit(int);
+9  void jack_bauer(void);
+10 void times_table(void);
+11 int add(int, int);
+12 void print_to_98(int n);
