5896cb2ad23bf37494ef34720e80295694b7cc90
DOREENDAVID <doreenkinya153@gmail.com>
2023-03-15 11:59:36 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,34 +1,34 @@
-[200~#include <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- *  * _strdup - copies a string into a new buffer
- *   * @str: the string to copy
- *    * Return: pointer to a new buffer
- *     *
- *      */
+ * _strdup - copies a string into a new buffer
+ * @str: the string to copy
+ * Return: pointer to a new buffer
+ *
+ */
 
 char *_strdup(char *str)
 {
-	        char *p;
-		        unsigned int i, j;
+	char *p;
+	unsigned int i, j;
 
-			        if (str == NULL)
-					                return (NULL);
+	if (str == NULL)
+	return (NULL);
 
-				        for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
-					        if (i < 1)
-							                return (NULL);
-						        p = malloc(i *sizeof(char));
-							        if (p == NULL)
-									        {
-											                free(p);
-													                return (NULL);
-															        }
-								        for (j = 0; j < i; j++)
-
-										                p[j] = str[j];
-									        p[j] = '\0';
-										        return (p);
+	if (i < 1)
+	return (NULL);
+	p = malloc(i * sizeof(char));
+	if (p == NULL)
+	{
+	free(p);
+	return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	p[j] = str[j];
+	p[j] = '\0';
+	return (p);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * free_grid - a function that frees a 2-D grid previously created by alloc_grid
+ * free_grid - a function that frees a 2-D grid 
+ * previously created by alloc_grid
  * @grid: pointer to grid to free
  * @height: height of grid
  */
