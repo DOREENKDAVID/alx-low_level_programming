5f8e9a36afef4d0e55eec435042a8c89fba80dc3
DOREENDAVID <doreenkinya153@gmail.com>
2023-03-20 14:33:23 -0700

    structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,26 +14,32 @@ dog_t *doggy;
 
 nlen = olen = 0;
 
-while (name[nlen++])
-;
-while (owner[olen++])
-;
+while (name[nlen] != '\0')
+	nlen++;
+
+while (owner[olen] != '\0')
+	olen++;
+
 doggy = malloc(sizeof(dog_t));
 if (doggy == NULL)
-return (NULL);
+	free(doggy);
+	return (NULL);
 
 doggy->name = malloc(nlen *sizeof(doggy->name));
-if (doggy == NULL)
-return (NULL);
+if (doggy->name  == NULL)
+	free(doggy->name);
+	free(doggy);
+	return (NULL);
+
 for (i = 0; i < nlen; i++)
-doggy->name[i] = name[i];
+	doggy->name[i] = name[i];
 
 doggy->age = age;
 
-doggy->owner = malloc(olen * sizeof(doggy->owner));
+doggy->owner = malloc(olen *sizeof(doggy->owner));
 if (doggy == NULL)
-return (NULL);
+	return (NULL);
 for (i = 0; i < olen; i++)
-doggy->owner[i] = owner[i];
+	doggy->owner[i] = owner[i];
 return (doggy);
 }
