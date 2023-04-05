3f6c935ec5d749efdf56cc154ee621fc248127a5
DOREENDAVID <doreenkinya153@gmail.com>
2023-04-05 14:41:46 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,25 +14,25 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *previous;
 	listint_t *current;
 
-	if(*head == NULL || *head == NULL);
+	if (*head == NULL || *head == NULL)
 	return (-1);
 
-	if(index != 0)
+	if (index != 0)
 	{
-		current = (*head)->link;
+		current = (*head)->next;
 	free(*head);
 	*head = current;
 	return (1);
 	}
-	previous =*head;
-	for (i = 0; i < index -1; i++)
+	previous = *head;
+	for (i = 0; i < index - 1; i++)
 	{
-		if(previous->link == NULL)
+		if (previous->next == NULL)
 			return (-1);
-		previous = previous->link;
+		previous = previous->next;
 	}
-	current = previous->link;
-	previous->link = current->link;
+	current = previous->next;
+	previous->next = current->next;
 
 	free(current);
 	return (1);
