063af84a75b8434de186bcf021cbf8325973fe35
DOREENDAVID <doreenkinya153@gmail.com>
2023-04-05 14:14:37 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -16,7 +16,7 @@ int pop_listint(listint_t **head)
 	if (head ==  NULL || *head == NULL)
 		return (0);
 	temp = *head;
-	head = head->next;
+	head = temp->next;
 	n = temp->n;
 	free(temp);
 	return (n);
