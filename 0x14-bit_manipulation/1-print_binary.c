4e4541e9ee7ba89eb046d1746554f98c6fc22411
DOREENDAVID <doreenkinya153@gmail.com>
2023-04-08 00:07:05 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,18 +3,15 @@
 
 /**
  * print_binary - a fun tha prints binary rep of a number.
- * @n the int to be printed
+ * @n: the int to be printed
  * return: 0
  */
 
 void print_binary(unsigned long int n)
 {
-
-	
-	for (int 1 = 31; i .=0; i--)
+	for (int i = 31; i >= 0; i--)
 	{
-		putchar((n & (1 <<i)) ? '1' : '0');
+		putchar((n & (1 << i)) ? '1' : '0');
 	}
-	putchar('\n');
+	_putchar('\n');
 }
-		
