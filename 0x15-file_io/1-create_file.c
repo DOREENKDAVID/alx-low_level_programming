0c7c7730e99efc812127c4760cc8195f1844d8fa
DOREENDAVID <doreenkinya153@gmail.com>
2023-04-09 10:07:13 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,12 +32,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	return (-1);
+
 	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	if (fp == -1)
 	return (-1);
+
 	if (text_content != NULL)
 	len = write(fp, text_content, _strlen(text_content));
 	close(fp);
+
 	if (len == -1)
 	return (-1);
 	return (1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,6 @@ size_t _strlen(char *str)
 	size_t i;
 
 	for (i = 0; str[i]; i++)
-
 	return (i);
 }
 
@@ -35,12 +34,16 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	return (-1);
+
 	fp = open(filename, O_WRONLY | O_APPEND);
+
 	if (fp == -1)
 	return (-1);
+
 	if (text_content != NULL)
 	len = write(fp, text_content, _strlen(text_content));
 	close(fp);
+
 	if (len == -1)
 	return (-1);
 	return (1);
