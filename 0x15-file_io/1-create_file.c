39c4e6ece9e237e3ba6f3b26e729162a7d69fa63
DOREENDAVID <doreenkinya153@gmail.com>
2023-04-09 10:31:06 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,7 +34,6 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 
 	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
 	if (fp == -1)
 	return (-1);
 
