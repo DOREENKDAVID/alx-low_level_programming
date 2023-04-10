05d65bee4a39b6fb0c8ed09b7aba15360f321088
DOREENDAVID <doreenkinya153@gmail.com>
2023-04-10 03:25:13 -0700

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -35,14 +35,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 	fp = open(filename, O_WRONLY | O_APPEND);
-
 	if (fp == -1)
 	return (-1);
-
 	if (text_content != NULL)
 	len = write(fp, text_content, _strlen(text_content));
 	close(fp);
-
 	if (len == -1)
 	return (-1);
 	return (1);
